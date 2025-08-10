6fec76c1188745b86382e97b13a4ef57a1712c66
Dev Goswami <goswami.dev200@gmail.com>
2025-08-10 06:04:01 +0000

    footer is still is not visible

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -8,13 +8,15 @@ export default function WebLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col bg-background relative">
-      <div id="golden-particles" />
-      <Header />
-      <main className="flex-1">
-        <PageTransition>{children}</PageTransition>
-      </main>
+    <>
+      <div className="flex min-h-screen flex-col bg-background relative">
+        <div id="golden-particles" />
+        <Header />
+        <main className="flex-1">
+          <PageTransition>{children}</PageTransition>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
